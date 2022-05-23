7c18f40c811c3ec10e04476613d272788749a633
eryan411 <eryan411@gmail.com>
2022-05-22 23:27:12 -0400

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,9 @@
+// GLOBALS    - NO WINDOW!!!!
+
+// __dirname  - path to current directors
+// __filename - file name
+// require    - function to use modules (commonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log(module)
